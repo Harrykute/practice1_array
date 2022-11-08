4095d313c796b5752b1df020fd2024851d7229b0
Harry <kuterharish@gmail.com>
2022-11-08 14:41:51 +0530

    first commit

---
+++ Practice1_02.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+
+public class Practice1_02 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       System.out.println("enter the side of square");
+         int side= sc.nextInt();
+          double areasq= side*side;
+
+
+          System.out.println("area of squear"+areasq);
+
+
+
+
+
+
+    }
+
+
+
+
+}
